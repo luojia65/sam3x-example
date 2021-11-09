bf94cd7d1173a201336c541ac606c3afa47b699d
luojia65 <me@luojia.cc>
2021-11-09 19:25:31 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+You need to install BOSSA to flash binary file into target.
---
+++ src/main.rs
@@ -0,0 +1,54 @@
+#![no_std]
+#![no_main]
+
+extern crate panic_halt; // you can put a breakpoint on `rust_begin_unwind` to catch panics
+extern crate sam3x8e;
+
+use cortex_m_rt::entry;
+use sam3x8e::RTT;
+
+fn delay_ms(rtt: &RTT, ms: u32) {
+    // We're not considering overflow here, 32 bits can keep about 49 days in ms
+    let now = rtt.vr.read().bits();
+    let until = now + ms;
+
+    while rtt.vr.read().bits() < until {}
+}
+
+#[entry]
+fn main() -> ! {
+    let p = sam3x8e::Peripherals::take().unwrap();
+
+    // Enable PIOB
+    let pmc = p.PMC;
+    unsafe {
+        pmc.pmc_pcer0.write_with_zero(|w| w.pid12().set_bit());
+    }
+
+    // Configure RTT resolution to approx. 1ms
+    let rtt = p.RTT;
+    unsafe {
+        rtt.mr.write(|w|  w.rtpres().bits(0x20));
+    }
+
+    let piob = p.PIOB;
+
+    // Configure PIOB pin 27 (LED)
+    unsafe {
+        piob.per.write_with_zero(|w| w.p27().set_bit());
+        piob.oer.write_with_zero(|w| w.p27().set_bit());
+        piob.pudr.write_with_zero(|w| w.p27().set_bit());
+    }
+
+    // On/off blinking
+    loop {
+        unsafe {
+            piob.sodr.write_with_zero(|w| w.p27().set_bit());
+        }
+        delay_ms(&rtt, 1000);
+        unsafe {
+            piob.codr.write_with_zero(|w| w.p27().set_bit());
+        }
+        delay_ms(&rtt, 1000);
+    }
+}
